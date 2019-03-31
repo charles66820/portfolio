0c39be21a86a8b7c3b43f86efb8f2f0d100e0a32
charles66820 <charles.goedefroit@gmail.com>
2019-03-31 18:00:48 +0200

    motd

--- public/js/txtType.js
+++ public/js/txtType.js
@@ -35,13 +35,16 @@ TxtType.prototype.tick = function() {
   }, delta);
 };
 
-window.onload = function() {
+window.addEventListener("load", function(){
   var elements = document.getElementsByClassName('typewrite');
   for (var i=0; i<elements.length; i++) {
     var toRotate = elements[i].getAttribute('data-type');
+    if (toRotate == "des") {
+      toRotate = "[\""+elements[i].innerText+"\"]";
+    }
     var period = elements[i].getAttribute('data-period');
     if (toRotate) {
       new TxtType(elements[i], JSON.parse(toRotate), period);
     }
   }
-};
+}, false);
