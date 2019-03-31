b29d1cc3f46b9840445b629cf2cde2cd25819b30
charles66820 <charles.goedefroit@gmail.com>
2019-03-31 01:14:14 +0100

    les infosssss

--- src/routes.php
+++ src/routes.php
@@ -7,28 +7,44 @@ use Slim\Http\StatusCode;
 // Routes
 $app->get('/', function (Request $req, Response $res) {
 
-  return $this->view->render($res, 'about.html.twig');
+  return $this->view->render($res, 'about.html.twig', [
+    "title" => "Accueil"
+  ]);
 });
 
 $app->get('/about', function (Request $req, Response $res) {
 
-  return $this->view->render($res, 'about.html.twig');
+  return $this->view->render($res, 'about.html.twig', [
+    "title" => "Accueil"
+  ]);
 });
 
-$app->get('/contact', function (Request $req, Response $res) {
+$app->get('/E6', function (Request $req, Response $res) {
 
-  return $this->view->render($res, 'contact.html.twig');
+  return $this->view->render($res, 'E6.html.twig', [
+    "title" => "Épreuve E6"
+  ]);
 });
 
 $app->get('/projects', function (Request $req, Response $res) {
 
-  return $this->view->render($res, 'projects.html.twig');
+  return $this->view->render($res, 'projects.html.twig', [
+    "title" => "Mes projets"
+  ]);
+});
+
+$app->get('/contact', function (Request $req, Response $res) {
+
+  return $this->view->render($res, 'contact.html.twig', [
+    "title" => "Contact"
+  ]);
 });
 
 $app->get('/project/{id}', function (Request $req, Response $res, array $args) {
 
   return $this->view->render($res, 'project.html.twig', [
-    'id' => $args['id']
+    'id' => $args['id'],
+    "title" => $args['id']
   ]);
 });
 
@@ -39,7 +55,7 @@ $app->get('/test', function (Request $req, Response $res) {
   ]);
 });
 
-
+/*
 //Error handlers
 $c = $app->getContainer();
 $c['errorHandler'] = function ($c) {
@@ -48,12 +64,13 @@ $c['errorHandler'] = function ($c) {
     $res = $res->withStatus(StatusCode::HTTP_INTERNAL_SERVER_ERROR)->withHeader('Content-Type', 'text/html');
 
     return $c['view']->render($res, 'error.html.twig', [
-    'code' => 500,
-    'msg' => "une erreur c'est produite :( , veuillez réessayer us trader.",
-    'details' => json_encode($e)
+      'title' => "Erreur 500",
+      'code' => 500,
+      'msg' => "une erreur c'est produite :( , veuillez réessayer us trader.",
+      'details' => json_encode($e)
     ]);
   };
-};
+};*/
 
 $c['notFoundHandler'] = function ($c) {
   return function (Request $req, Response $res) use ($c) {
@@ -61,8 +78,9 @@ $c['notFoundHandler'] = function ($c) {
     $res = $res->withStatus(StatusCode::HTTP_NOT_FOUND);
 
     return $c['view']->render($res, 'error.html.twig', [
-    'code' => 404,
-    'msg' => "Cette page n'existe pas :("
+      'title' => "Erreur 404",
+      'code' => 404,
+      'msg' => "Cette page n'existe pas :("
     ]);
   };
 };
