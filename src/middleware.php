878190c5e54faa338f64e5b61a12a3c2cb6a4e0e
charles66820 <charles.goedefroit@gmail.com>
2020-09-12 23:13:31 +0200

    Implement langs

--- src/middleware.php
+++ src/middleware.php
@@ -2,3 +2,29 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+$langs = [ 'fr', 'en' ];
+$local = [
+  "en" => json_decode(file_get_contents(__DIR__ . '/data_en.json')),
+  "fr" => json_decode(file_get_contents(__DIR__ . '/data_fr.json'))
+];
+
+// Language selection
+$app->add(function ($req, $res, $next) use ($local, $langs) {
+
+  // If lang cookie is set
+  if (isset($_COOKIE["lang"])) $lang = $_COOKIE["lang"];
+  else {
+    // default if lang cookie not set
+    $lang = $req->getHeader('accept-language')[0];
+    $lang = explode(',', $lang)[0];
+    $lang = explode('-', $lang)[0];
+  }
+
+  // If bad lang set to en
+  if (!in_array($lang, $langs)) $lang = 'en';
+
+  $req = $req->withAttribute('accept-language', $lang);
+  $req->local["data"] = $local[$lang];
+	return $next($req, $res);
+});
--- src/routes.php
+++ src/routes.php
@@ -6,133 +6,53 @@ use Slim\Http\StatusCode;
 
 // Routes
 $app->get('/', function (Request $req, Response $res) {
-  return $res->withRedirect('/about', 307);
+  return $this->view->render($res, 'home.html.twig', [
+    "title" => $req->local["data"]->titles->home,
+    "data" => $req->local["data"]
+  ]);
 });
 
 $app->get('/about', function (Request $req, Response $res) {
-
   return $this->view->render($res, 'about.html.twig', [
-    "title" => "Me concernant"
+    "title" => $req->local["data"]->titles->about,
+    "data" => $req->local["data"]
   ]);
 });
 
 $app->get('/projects', function (Request $req, Response $res) {
-
   return $this->view->render($res, 'projects.html.twig', [
-    "title" => "Mes projets"
+    "title" => $req->local["data"]->titles->projects,
+    "data" => $req->local["data"]
   ]);
 });
 
 $app->get('/contact', function (Request $req, Response $res) {
-
   return $this->view->render($res, 'contact.html.twig', [
-    "title" => "Contact"
+    "title" => $req->local["data"]->titles->contact,
+    "data" => $req->local["data"]
   ]);
 });
 
-$app->get('/project/OTako', function (Request $req, Response $res, array $args) {
+$app->get('/project/{name}', function (Request $req, Response $res, array $args) {
+  $index = array_search($args["name"], array_column($req->local["data"]->projects, "name"));
+  if (!$index)
+    return $this->view->render($res, 'error.html.twig', [
+      'title' => $req->local["data"]->errors->error . "404",
+      'code' => 404,
+      'msg' => $req->local["data"]->errors->projectNotFound,
+      'name' => $args['name'],
+      "data" => $req->local["data"]
+    ]);
 
+  $project = $req->local["data"]->projects[$index];
   return $this->view->render($res, 'project.html.twig', [
-    'id' => $args['id'],
-    "title" => "Projet Ô' Tako",
-    "projet" => [
-      "name" => "Ô' Tako",
-      "description" => "Ô' Tako est un site de vente de goodis.
-      Sur celui-ci il est possible de consulter les produits, de créer un compte, d’ajouter des produits au panier et de commander.",
-      "imgs" => [
-        [
-          "src" => "/img/projects/OTako/ppe3ScreenFull.png"
-        ],
-        [
-          "src" => "/img/projects/OTako/screen1.png"
-        ]
-      ],
-      "docU" => [
-        "url" => "/dl/DocumentationUtilisateurOTako.pdf",
-        "name" => "Documentation utilisateur Ô' Tako"
-      ],
-      "docT" => [
-        "url" => "/dl/DocumentationTechniqueOTako.pdf",
-        "name" => "Documentation technique Ô' Tako"
-      ],
-      "code" => [
-        "url" => "/dl/OTakoSourceCode.zip",
-        "name" => "Ô' Tako code source"
-      ],
-      "git" => [
-        "url" => "https://github.com/charles66820/ppe3",
-        "name" => "Voire sur Github"
-      ],
-      "build" => [
-        "lbl" => "Accéder au site",
-        "url" => "https://ppe3.magicorp.fr/",
-        "name" => "Ô' Tako"
-      ],
-      "dossierDrive" => [
-        "url" => "https://drive.google.com/drive/folders/1s12xr3EbPmE5juf-7NMtzh2-qtiMuGwe?usp=sharing",
-        "name" => "Dossier situation 1"
-      ]
-    ]
-  ]);
-});
-
-$app->get('/project/OTactile', function (Request $req, Response $res, array $args) {
-
-  return $this->view->render($res, 'project.html.twig', [
-    'id' => $args['id'],
-    "title" => "Projet Ô' Tactile",
-    "projet" => [
-      "name" => "Ô' Tactile",
-      "description" => "Ô' Tactile est une application de vente de goodis.
-      Elle permet de consulter les produits, consulter son profile et de gérer le stock d’un produit.",
-      "imgs" => [
-        [
-          "src" => "/img/projects/OTactile/screen1.png"
-        ],
-        [
-          "src" => "/img/projects/OTactile/screen2.png"
-        ]
-      ],
-      "docU" => [
-        "url" => "/dl/DocumentationUtilisateurOTactile.pdf",
-        "name" => "Documentation utilisateur Ô' Tactile"
-      ],
-      "docT" => [
-        "url" => "/dl/DocumentationTechniqueOTactile.pdf",
-        "name" => "Documentation technique Ô' Tactile"
-      ],
-      "code" => [
-        "url" => "/dl/OTactileSourceCode.zip",
-        "name" => "Ô' Tactile code source"
-      ],
-      "git" => [
-        "url" => "https://github.com/charles66820/OTactile",
-        "name" => "Voire sur Github"
-      ],
-      "build" => [
-        "lbl" => "Lien de téléchargement de l'apk",
-        "url" => "/dl/OTactile.apk",
-        "name" => "apk Ô' Tactile"
-      ],
-      "dossierDrive" => [
-        "url" => "https://drive.google.com/drive/folders/181gcj8rbZrXH3fJbC5N8eyK_P3IN8pZ5?usp=sharing",
-        "name" => "Dossier situation 2"
-      ]
-    ]
-  ]);
-});
-
-$app->get('/project/{id}', function (Request $req, Response $res, array $args) {
-
-  return $this->view->render($res, 'error.html.twig', [
-    'title' => "Erreur 404",
-    'code' => 404,
-    'msg' => "le projet ".$args['id']." n'existe pas ou n'a pas encore été docummanté."
+    "title" => "Projet " . $project->title,
+    "projet" => $project,
+    "data" => $req->local["data"]
   ]);
 });
 
 $app->get('/veilletechnologique', function (Request $req, Response $res) {
-
   return $this->view->render($res, 'veilletechnologique.html.twig', [
     'title' => "Veille technologique"
   ]);
@@ -147,9 +67,9 @@ if (getenv('APP_ENV') != 'dev') {
       $res = $res->withStatus(StatusCode::HTTP_INTERNAL_SERVER_ERROR)->withHeader('Content-Type', 'text/html');
 
       return $c['view']->render($res, 'error.html.twig', [
-        'title' => "Erreur 500",
+        'title' => $req->local["data"]->errors->error . "500",
         'code' => 500,
-        'msg' => "une erreur c'est produite :( , veuillez réessayer us trader.",
+        'msg' => $req->local["data"]->errors->serverError,
         'details' => json_encode($e)
       ]);
     };
@@ -161,9 +81,9 @@ if (getenv('APP_ENV') != 'dev') {
       $res = $res->withStatus(StatusCode::HTTP_NOT_FOUND);
 
       return $c['view']->render($res, 'error.html.twig', [
-        'title' => "Erreur 404",
+        'title' =>  $req->local["data"]->errors->error . "404",
         'code' => 404,
-        'msg' => "Cette page n'existe pas :("
+        'msg' => $req->local["data"]->errors->pageNotFound
       ]);
     };
   };
